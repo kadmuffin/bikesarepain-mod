84fa291ccd670e7ab1b2c892527cb90359525e98
Kevin Ledesma <46900861+kadmuffin@users.noreply.github.com>
2024-11-20 21:37:20 -0300

    chore: Move around tooltip elements

--- common/src/main/java/com/kadmuffin/bikesarepain/client/item/TooltipManager.java
+++ common/src/main/java/com/kadmuffin/bikesarepain/client/item/TooltipManager.java
@@ -16,6 +16,23 @@ import java.util.Objects;
 public class TooltipManager {
     public static void init() {
         ClientTooltipEvent.ITEM.register((stack, lines, context, tooltipFlag) -> {
+            if (stack.getItem() instanceof BikeItem) {
+                int durability = stack.getDamageValue();
+                int maxDurability = stack.getMaxDamage();
+
+                // We will show a percentage of durability left
+                int durabilityPercentage = (int) (100 - (((float) durability / (float) maxDurability) * 100));
+
+                lines.add(2, Component.translatable("item.bikesarepain.bicycle.tooltip.health")
+                        .withColor(CommonColors.GRAY)
+                        .append(Component.literal(durabilityPercentage + "%")
+                                .withStyle(durabilityPercentage == 100 ? net.minecraft.ChatFormatting.GREEN
+                                        : durabilityPercentage > 75 ? net.minecraft.ChatFormatting.DARK_GREEN
+                                        : durabilityPercentage > 50 ? net.minecraft.ChatFormatting.YELLOW
+                                        : durabilityPercentage > 25 ? net.minecraft.ChatFormatting.RED
+                                        : net.minecraft.ChatFormatting.DARK_RED)));
+            }
+            
             if (stack.has(ComponentManager.SAVE_TIME.get()) && Boolean.TRUE.equals(stack.get(ComponentManager.SAVE_TIME.get()))) {
                 if (stack.has(ComponentManager.TICKS_MOVED.get())) {
                     // Unpack in safe way
@@ -63,40 +80,15 @@ public class TooltipManager {
                 }
             }
 
-            if (stack.getItem() instanceof BikeItem) {
-                int durability = stack.getDamageValue();
-                int maxDurability = stack.getMaxDamage();
-
-                // We will show a percentage of durability left
-                int durabilityPercentage = (int) (100 - (((float) durability / (float) maxDurability) * 100));
-
-                if (durabilityPercentage == 100) {
-                    lines.add(Component.translatable("item.bikesarepain.bicycle.tooltip.brand_new")
-                            .withColor(CommonColors.GREEN));
-                } else {
-                    lines.add(Component.translatable("item.bikesarepain.bicycle.tooltip.repair")
-                            .withColor(CommonColors.GREEN));
-                }
-
-                // Check if contains a saddle
-                if (stack.has(ComponentManager.SADDLED.get()) && Boolean.TRUE.equals(stack.get(ComponentManager.SADDLED.get()))) {
-                    lines.add(Component.translatable("item.bikesarepain.bicycle.tooltip.saddled")
-                            .withColor(CommonColors.GRAY).append(Component.translatable("item.bikesarepain.bicycle.tooltip.yes")
-                                    .withColor(Color.ofRGB(255, 149, 0).argbInt())));
-                } else {
-                    lines.add(Component.translatable("item.bikesarepain.bicycle.tooltip.saddled")
-                            .withColor(CommonColors.GRAY).append(Component.translatable("item.bikesarepain.bicycle.tooltip.no")
-                                    .withColor(CommonColors.RED)));
-                }
-
-                lines.add(2, Component.translatable("item.bikesarepain.bicycle.tooltip.health")
-                        .withColor(CommonColors.GRAY)
-                        .append(Component.literal(durabilityPercentage + "%")
-                                .withStyle(durabilityPercentage == 100 ? net.minecraft.ChatFormatting.GREEN
-                                        : durabilityPercentage > 75 ? net.minecraft.ChatFormatting.DARK_GREEN
-                                        : durabilityPercentage > 50 ? net.minecraft.ChatFormatting.YELLOW
-                                        : durabilityPercentage > 25 ? net.minecraft.ChatFormatting.RED
-                                        : net.minecraft.ChatFormatting.DARK_RED)));
+            // Check if contains a saddle
+            if (stack.has(ComponentManager.SADDLED.get()) && Boolean.TRUE.equals(stack.get(ComponentManager.SADDLED.get()))) {
+                lines.add(Component.translatable("item.bikesarepain.bicycle.tooltip.saddled")
+                        .withColor(CommonColors.GRAY).append(Component.translatable("item.bikesarepain.bicycle.tooltip.yes")
+                                .withColor(Color.ofRGB(255, 149, 0).argbInt())));
+            } else {
+                lines.add(Component.translatable("item.bikesarepain.bicycle.tooltip.saddled")
+                        .withColor(CommonColors.GRAY).append(Component.translatable("item.bikesarepain.bicycle.tooltip.no")
+                                .withColor(CommonColors.RED)));
             }
         });
     }

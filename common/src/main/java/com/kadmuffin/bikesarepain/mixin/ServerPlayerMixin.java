9e0d1da1757a2a6be4420e8ab4941ad2f85d47b9
Kevin Ledesma <46900861+kadmuffin@users.noreply.github.com>
2024-11-27 23:25:00 -0300

    fix: fix stats for distance built jars

--- common/src/main/java/com/kadmuffin/bikesarepain/mixin/ServerPlayerMixin.java
+++ common/src/main/java/com/kadmuffin/bikesarepain/mixin/ServerPlayerMixin.java
@@ -1,30 +1,36 @@
 package com.kadmuffin.bikesarepain.mixin;
 
-import com.kadmuffin.bikesarepain.accessor.EntityInvoker;
-import com.kadmuffin.bikesarepain.accessor.PlayerInvoker;
 import com.kadmuffin.bikesarepain.server.StatsManager;
 import com.kadmuffin.bikesarepain.server.entity.AbstractBike;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.StatFormatter;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ServerPlayer.class)
-public abstract class ServerPlayerMixin implements PlayerInvoker, EntityInvoker {
+public abstract class ServerPlayerMixin {
+
+    @Shadow public abstract void awardStat(Stat<?> stat, int increment);
 
     @Inject(
             method = "checkRidingStatistics(DDD)V",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/server/level/ServerPlayer;getVehicle()Lnet/minecraft/world/entity/Entity;"
-            ),
-            cancellable = true)
-    private void beforeAbstractHorseCheck(double dx, double dy, double dz, CallbackInfo ci) {
-        Entity entity = this.getVehicle();
+            at = @At(value = "FIELD",
+                    target = "Lnet/minecraft/stats/Stats;HORSE_ONE_CM:Lnet/minecraft/resources/ResourceLocation;",
+                    shift = At.Shift.BEFORE),
+            locals = LocalCapture.CAPTURE_FAILSOFT,
+            cancellable = true
+    )
+    private void onGetVehicle(double dx, double dy, double dz, CallbackInfo ci, @Local Entity entity, @Local int i) {
         if (entity instanceof AbstractBike) {
-            this.awardStat(StatsManager.DISTANCE_TRAVELED, Math.round((float)Math.sqrt(dx * dx + dy * dy + dz * dz) * 100.0F));
+            this.awardStat(Stats.CUSTOM.get(StatsManager.DISTANCE_TRAVELED), i);
             ci.cancel();
         }
     }
--- common/src/main/java/com/kadmuffin/bikesarepain/server/StatsManager.java
+++ common/src/main/java/com/kadmuffin/bikesarepain/server/StatsManager.java
@@ -7,15 +7,9 @@ import net.minecraft.stats.Stats;
 
 public class StatsManager {
     public static final ResourceLocation DISTANCE_TRAVELED = ResourceLocation.fromNamespaceAndPath(BikesArePain.MOD_ID, "distance_traveled");
-    // JSerialComm
-    public static final ResourceLocation DISTANCE_TRAVELED_JSC = ResourceLocation.fromNamespaceAndPath(BikesArePain.MOD_ID, "distance_traveled_jsc");
-    public static final ResourceLocation CALORIES_BURNED_JSC = ResourceLocation.fromNamespaceAndPath(BikesArePain.MOD_ID, "calories_burned_jsc");
 
     public static void init() {
         BikesArePain.STATS.register(DISTANCE_TRAVELED.getPath(), () -> DISTANCE_TRAVELED);
-        BikesArePain.STATS.register(DISTANCE_TRAVELED_JSC.getPath(), () -> DISTANCE_TRAVELED_JSC);
-        BikesArePain.STATS.register(CALORIES_BURNED_JSC.getPath(), () -> CALORIES_BURNED_JSC);
-
         BikesArePain.STATS.register();
 
         Stats.CUSTOM.get(DISTANCE_TRAVELED, StatFormatter.DISTANCE);

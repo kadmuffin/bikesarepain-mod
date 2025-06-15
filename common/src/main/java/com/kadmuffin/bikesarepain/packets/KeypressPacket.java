1ad3fbb714bfc91f11ed561dbc28f0ca2ca54515
Kadmuffin <kadmuffin@outlook.com>
2025-06-15 20:16:16 -0300

    refactor(KeypressPacket): improve readability and separate concerns of key types

--- common/src/main/java/com/kadmuffin/bikesarepain/packets/KeypressPacket.java
+++ common/src/main/java/com/kadmuffin/bikesarepain/packets/KeypressPacket.java
@@ -9,98 +9,78 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static com.kadmuffin.bikesarepain.BikesArePain.MOD_ID;
 
 public class KeypressPacket {
+
+    @FunctionalInterface
+    public interface KeyAction {
+        void execute(boolean isPressed, Bicycle bike);
+    }
+
     public enum KeyType {
-        RING_BELL(0),
-        BRAKE(1),
-        SWITCHD(2);
+        RING_BELL(0, (isPressed, bike) -> {
+            if (isPressed != bike.isRingAlreadyPressed()) {
+                bike.setRingAlreadyPressed(isPressed);
+                if (isPressed) {
+                    bike.ringBell();
+                }
+            }
+        }),
+        BRAKE(1, (isPressed, bike) -> {
+            if (isPressed != bike.isBraking()) {
+                bike.setBraking(isPressed);
+            }
+        }),
+        // Switching the current display category
+        SWITCHD(2, (isPressed, bike) -> {
+            if (isPressed) {
+                bike.chooseNextDisplayStat();
+            }
+        });
 
         private final int type;
+        private final KeyAction action;
+        private static final Map<Integer, KeyType> BY_TYPE =
+                Arrays.stream(values()).collect(Collectors.toUnmodifiableMap(KeyType::getType, key -> key));
 
-        KeyType(int type) {
+        KeyType(int type, KeyAction action) {
             this.type = type;
+            this.action = action;
         }
 
         public static KeyType fromType(int type) {
-            for (KeyType key : values()) {
-                if (key.getType() == type) {
-                    return key;
-                }
-            }
-            return null;
+            return BY_TYPE.get(type);
         }
 
-        // Get the type of the keypress
         public int getType() {
-            return type;
-        }
-
-        public void ringBell(boolean isPressed, NetworkManager.PacketContext context) {
-            Player player = context.getPlayer();
-            if (player != null) {
-                Bicycle bike = player.getVehicle() instanceof Bicycle ? (Bicycle) player.getVehicle() : null;
-                if (bike != null) {
-                    if (isPressed != bike.isRingAlreadyPressed()) {
-                        boolean newRingState = !bike.isRingAlreadyPressed();
-                        bike.setRingAlreadyPressed(newRingState);
-                        if (newRingState) {
-                            bike.ringBell();
-                        }
-                    }
-                }
-            }
-        }
-
-        public void switchDisplay(boolean isPressed, NetworkManager.PacketContext context) {
-            Player player = context.getPlayer();
-            if (player != null) {
-                Bicycle bike = player.getVehicle() instanceof Bicycle ? (Bicycle) player.getVehicle() : null;
-                if (bike != null) {
-                    if (isPressed) {
-                        bike.chooseNextDisplayStat();
-                    }
-                }
-            }
-        }
-
-        public void brake(boolean isPressed, NetworkManager.PacketContext context) {
-            Player player = context.getPlayer();
-            if (player != null) {
-                Bicycle bike = player.getVehicle() instanceof Bicycle ? (Bicycle) player.getVehicle() : null;
-                if (bike != null) {
-                    if (isPressed != bike.isBraking()) {
-                        boolean newBrakeState = !bike.isBraking();
-                        bike.setBraking(newBrakeState);
-                    }
-                }
-            }
+            return this.type;
         }
 
         public void run(boolean isPressed, NetworkManager.PacketContext context) {
-            switch (this) {
-                case RING_BELL:
-                    ringBell(isPressed, context);
-                    break;
-                case BRAKE:
-                    brake(isPressed, context);
-                    break;
-                case SWITCHD:
-                    switchDisplay(isPressed, context);
-                    break;
+            Player player = context.getPlayer();
+            if (player != null && player.getVehicle() instanceof Bicycle bike) {
+                this.action.execute(isPressed, bike);
             }
         }
     }
 
     public record Packet(boolean isPressed, KeyType keyEnum) implements CustomPacketPayload {
-        public static final CustomPacketPayload.Type<Packet> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath(MOD_ID, "keypress_packet"));
-        public static final StreamCodec<FriendlyByteBuf, Packet> CODEC = StreamCodec.of((buf, obj) -> {
-            buf.writeBoolean(obj.isPressed);
-            buf.writeEnum(obj.keyEnum);
-        }, buf -> new KeypressPacket.Packet(buf.readBoolean(), buf.readEnum(KeyType.class)));
+        public static final Type<Packet> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(MOD_ID, "keypress_packet"));
+        public static final StreamCodec<FriendlyByteBuf, Packet> CODEC = StreamCodec.of(
+                (buf, packet) -> {
+                    buf.writeBoolean(packet.isPressed);
+                    buf.writeEnum(packet.keyEnum);
+                },
+                buf -> new Packet(buf.readBoolean(), buf.readEnum(KeyType.class))
+        );
 
-        public static final NetworkManager.NetworkReceiver<KeypressPacket.Packet> RECEIVER = (packet, contextSupplier) -> packet.keyEnum.run(packet.isPressed, contextSupplier);
+        public static final NetworkManager.NetworkReceiver<Packet> RECEIVER =
+                (packet, context) -> packet.keyEnum.run(packet.isPressed, context);
 
         @Override
         public @NotNull Type<? extends CustomPacketPayload> type() {

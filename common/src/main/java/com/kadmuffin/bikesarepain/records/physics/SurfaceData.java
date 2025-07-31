951237cb273609b9d99b6469fbaa7f4698b34cbe
Kadmuffin <kadmuffin@outlook.com>
2025-07-31 14:45:20 -0300

    feat: include BlockPos in SurfaceData

--- common/src/main/java/com/kadmuffin/bikesarepain/records/physics/SurfaceData.java
+++ common/src/main/java/com/kadmuffin/bikesarepain/records/physics/SurfaceData.java
@@ -3,21 +3,21 @@ package com.kadmuffin.bikesarepain.records.physics;
 import com.kadmuffin.bikesarepain.server.entity.AbstractBike;
 import net.minecraft.core.BlockPos;
 
-public record SurfaceData(float normalForce, float effectiveRollingCoeff, float effectiveStaticCoeff) {
+public record SurfaceData(BlockPos blockPos, float normalForce, float effectiveRollingCoeff, float effectiveStaticCoeff) {
 
     public static SurfaceData buildFrom(BikeState state, AbstractBike bike) {
         BlockPos blockPos = bike.getBlockPosBelowThatAffectsMyMovement();
-        float blockFrictionCoeff = 1 / bike.level()
+        float blockFrictionCoeff = bike.level()
                 .getBlockState(blockPos)
                 .getBlock()
                 .getFriction();
 
-        blockFrictionCoeff = Math.clamp(blockFrictionCoeff, 0.5f, 5.0f);
+        blockFrictionCoeff = Math.clamp(blockFrictionCoeff, 0.05f, 5.0f);
 
         float normalForce = state.totalMassKg() * state.gravityMps2();
         float effectiveRollingCoeff = 0.008f * blockFrictionCoeff;
-        float effectiveStaticCoeff = 0.1f * blockFrictionCoeff;
+        float effectiveStaticCoeff = 1.5f * effectiveRollingCoeff * blockFrictionCoeff;
 
-        return new SurfaceData(normalForce, effectiveRollingCoeff, effectiveStaticCoeff);
+        return new SurfaceData(blockPos, normalForce, effectiveRollingCoeff, effectiveStaticCoeff);
     }
 }
